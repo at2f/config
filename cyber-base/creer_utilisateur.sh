d142b1a4e0d4b519092799cf80dc04c8f55bc782
at2f <91329492+at2f@users.noreply.github.com>
2022-04-26 10:01:57 +0200

    Update creer_utilisateur.sh

--- cyber-base/creer_utilisateur.sh
+++ cyber-base/creer_utilisateur.sh
@@ -1,16 +1,18 @@
 #!/usr/bin/bash
 
+# Si l’assignation du mot de passe échoue, supprimmer l’utilisateur avec la commande suivante :
+# userdel -r 'login'
+
 if [[ $# != 2 ]] ; then
   echo "Usage: $0 login 'User name'"
   exit 1
 fi
 
-# Cr\u00e9er l\u2019utilisateur
+# Créer l’utilisateur
 useradd -m ${1} -s /bin/bash -c "${2}"
 
-# G\u00e9n\u00e9rer un mot de passe al\u00e9atoire
-user_password=$(printf "$(date)${RANDOM}" | md5sum | cut -d' ' -f1) 
+# Générer un mot de passe aléatoire
+user_password=$(printf "$(date)${RANDOM}" | md5sum | cut -d' ' -f1)
 
-# Assigner ce mot de passe \u00e0 l\u2019utilisateur
+# Assigner ce mot de passe à l’utilisateur
 echo "${1}:${user_password}" | chpasswd
-
