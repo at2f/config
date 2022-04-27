2625e5f1052a01a6e04221586f24d1cd236b0a3f
at2f <91329492+at2f@users.noreply.github.com>
2022-04-27 17:18:45 +0200

    Update corrections.sh

--- cyber-base/corrections.sh
+++ cyber-base/corrections.sh
@@ -7,4 +7,5 @@ echo 'Appliquation des corrections…'
 rm -f ~/installation_pc_cyber-base.sh
 
 sudo smbpasswd -x usager
+read -rp 'Entrer le mot de passe du partage SAMBA pour recevoir les scans : ' mdp_samba
 echo -e "${mdp_samba}\n${mdp_samba}\n" | sudo smbpasswd -aes usager
