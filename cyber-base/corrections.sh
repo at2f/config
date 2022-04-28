a06f14f28f96a3e6d50559b54d53eff9a936f164
at2f <91329492+at2f@users.noreply.github.com>
2022-04-28 15:06:40 +0200

    Update corrections.sh

--- cyber-base/corrections.sh
+++ cyber-base/corrections.sh
@@ -4,4 +4,4 @@
 # Elles seront appliquées lors de l’exécution du script de maintenance.
 
 echo 'Appliquation des corrections…'
-rm -f ~/installation_pc_cyber-base.sh
+rm -f ~/installation_postes_cyber-base.sh
