3e2c04477f4da4356527a70502bb96b5dc624be3
at2f <91329492+at2f@users.noreply.github.com>
2022-04-28 15:11:58 +0200

    Update corrections.sh

--- cyber-base/corrections.sh
+++ cyber-base/corrections.sh
@@ -6,4 +6,20 @@
 echo 'Appliquation des corrections…'
 rm -f ~/installation_pc_cyber-base.sh
 rm -f ~/installation_postes_cyber-base.sh
+# Créer le service qui lancera le script de déconnexion à l’extinction de l’ordinateur
+sudo tee /etc/systemd/system/deconnexion_teicee.service << EOF > /dev/null
+[Unit]
+Description=Lance le script de déconnexion du compte Téïcée
+After=network.target
+
+[Service]
+Type=oneshot
+RemainAfterExit=true
+ExecStop=/usr/local/sbin/deconnexion_teicee.sh
+
+[Install]
+WantedBy=multi-user.target
+EOF
+# Démarrer le service
+sudo systemctl enable deconnexion_teicee
 echo 'Corrections terminées.'
