548705b8dc40a2e0036cd98c61a9ecd84e178721
at2f <91329492+at2f@users.noreply.github.com>
2022-05-05 10:06:08 +0200

    Update corrections.sh

--- cyber-base/Postes usagers/divers/corrections.sh
+++ cyber-base/Postes usagers/divers/corrections.sh
@@ -6,7 +6,7 @@
 echo 'Appliquation des corrections…'
 ### AJOUTER LES CORRECTIONS APRÈS CETTE LIGNE ###
 
-sudo sed -i_bak 's/rights="none" pattern="PDF"/rights="read | write" pattern="PDF"/' /etc/ImageMagick-6/policy.xml
+#sudo sed -i_bak 's/rights="none" pattern="PDF"/rights="read | write" pattern="PDF"/' /etc/ImageMagick-6/policy.xml
 
 ### AJOUTER LES CORRECTIONS AVANT CETTE LIGNE ###
 echo 'Corrections terminées.'
