82b4001006198b938aa02a70117c09533cfa2141
at2f <91329492+at2f@users.noreply.github.com>
2022-05-06 11:00:18 +0200

    Update configurer_teicee.sh

--- cyber-base/Postes usagers/installation/modules/configurer_teicee.sh
+++ cyber-base/Postes usagers/installation/modules/configurer_teicee.sh
@@ -3,7 +3,7 @@
 # Installer le navigateur web en ligne de commande Lynx, nécessaire pour déconnecter les usagers à l’extinction du poste via le script « eteindre.sh »
 sudo apt install -y lynx
 # Créer le script de déconnexion du compte Téïcée
-sudo tee /usr/local/sbin/deconnexion_teicee.sh << EOF > /dev/null
+sudo tee /usr/local/sbin/deconnexion_ticncube.sh << EOF > /dev/null
 #!/usr/bin/bash
 
 # Liste les liens existants sur la page de connexion Téïcée
@@ -16,9 +16,9 @@ nohup lynx -dump -accept_all_cookies - &> /dev/null &
 sleep 5
 EOF
 # Rendre le script exécutable
-sudo chmod 755 /usr/local/sbin/deconnexion_teicee.sh
+sudo chmod 755 /usr/local/sbin/deconnexion_ticncube.sh
 # Créer le service qui lancera le script de déconnexion à l’extinction de l’ordinateur
-sudo tee /etc/systemd/system/deconnexion_teicee.service << EOF > /dev/null
+sudo tee /etc/systemd/system/deconnexion_ticncube.service << EOF > /dev/null
 [Unit]
 Description=Lance le script de déconnexion du compte Téïcée
 After=network.target
@@ -26,13 +26,13 @@ After=network.target
 [Service]
 Type=oneshot
 RemainAfterExit=true
-ExecStop=/usr/local/sbin/deconnexion_teicee.sh
+ExecStop=/usr/local/sbin/deconnexion_ticncube.sh
 
 [Install]
 WantedBy=multi-user.target
 EOF
 # Démarrer le service
-sudo systemctl enable deconnexion_teicee
+sudo systemctl enable deconnexion_ticncube
 # Régler le problème de fenêtre intempestive avec le filtrage réseau Téïcée
 sudo tee -a /etc/NetworkManager/NetworkManager.conf << EOF > /dev/null
 
