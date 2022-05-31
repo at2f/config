9f5e6ebe6b58c62d342e8cf801c2a99de2bdecf3
at2f <91329492+at2f@users.noreply.github.com>
2022-05-31 09:26:12 +0200

    Update configurer_explorateur_de_fichiers.sh

--- cyber-base/Postes usagers/installation/modules/configurer_explorateur_de_fichiers.sh
+++ cyber-base/Postes usagers/installation/modules/configurer_explorateur_de_fichiers.sh
@@ -4,14 +4,6 @@
 grep -qx snap ~/.hidden &> /dev/null || echo snap >> ~/.hidden
 # Supprime le répertoire « Public »
 rmdir ~/Public
-# Toujours afficher les vignettes d’aperçu des images/documents (y compris sur les périphériques externes)
-gsettings set org.gnome.nautilus.preferences show-image-thumbnails always
-# Afficher les vignettes pour les documents faisant 4 Go au maximum (au lieu de 512 Mo par défaut)
-gsettings set org.gnome.nautilus.preferences thumbnail-limit 4096
-# Lors d’une recherche, chercher dans les sous-dossiers également
-gsettings set org.gnome.nautilus.preferences recursive-search always
-# Toujours afficher le nombre de fichiers dans un dossier
-gsettings set org.gnome.nautilus.preferences show-directory-item-counts always
 # En mode liste, afficher les colonnes Nom, Taille, Type, et Dernière modification
 gsettings set org.gnome.nautilus.list-view default-visible-columns "['name', 'size', 'type', 'date_modified']"
 ## Ajouter des scripts au menu contextuel de l’explorateur de fichiers
