355901b8f5f08698a23590fb320d275d5746cbb8
at2f <91329492+at2f@users.noreply.github.com>
2022-05-31 09:27:26 +0200

    Update corrections.sh

--- cyber-base/Postes usagers/divers/corrections.sh
+++ cyber-base/Postes usagers/divers/corrections.sh
@@ -6,7 +6,14 @@
 echo 'Appliquation des corrections…'
 ### AJOUTER LES CORRECTIONS APRÈS CETTE LIGNE ###
 
-
+# Toujours afficher les vignettes d’aperçu des images/documents (y compris sur les périphériques externes)
+gsettings reset org.gnome.nautilus.preferences show-image-thumbnails
+# Afficher les vignettes pour les documents faisant 4 Go au maximum (au lieu de 512 Mo par défaut)
+gsettings reset org.gnome.nautilus.preferences thumbnail-limit
+# Lors d’une recherche, chercher dans les sous-dossiers également
+gsettings reset org.gnome.nautilus.preferences recursive-searchs
+# Toujours afficher le nombre de fichiers dans un dossier
+gsettings reset org.gnome.nautilus.preferences show-directory-item-counts
 
 ### AJOUTER LES CORRECTIONS AVANT CETTE LIGNE ###
 echo 'Corrections terminées.'
