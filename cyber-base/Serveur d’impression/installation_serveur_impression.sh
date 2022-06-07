03e5d6b499db226ae63111d55a9a07ce9c4caf67
at2f <91329492+at2f@users.noreply.github.com>
2022-06-07 16:31:24 +0200

    Update installation_serveur_impression.sh

--- cyber-base/Serveur d’impression/installation_serveur_impression.sh
+++ cyber-base/Serveur d’impression/installation_serveur_impression.sh
@@ -11,4 +11,14 @@ sudo systemctl enable --now cups
 sudo firewall-cmd --permanent --add-service=ipp
 sudo firewall-cmd --reload
 
-# IP fixe
+# Assigner une IP fixe
+cat << EOF > /etc/sysconfig/network-scripts/ifcfg-eth0
+DEVICE=eth0
+BOOTPROTO=none
+ONBOOT=yes
+PREFIX=24
+IPADDR=192.168.1.230
+EOF
+
+# Activer les mises à jour automatiques
+sudo sed -i 's/*upgrade_type*/upgrade_type = default/' /etc/dnf/automatic.conf
