1aead863f82b4d338e3b2ab4907174b3a5a877d2
at2f <91329492+at2f@users.noreply.github.com>
2022-06-21 17:41:42 +0200

    Update installation_serveur_impression.sh

--- cyber-base/Serveur d’impression/installation_serveur_impression.sh
+++ cyber-base/Serveur d’impression/installation_serveur_impression.sh
@@ -5,7 +5,7 @@ sudo hostnamectl set-hostname ServeurImpression
 
 # Activer Cockpit
 sudo systemctl enable --now cockpit.socket
-sudo firewall-cmd --permanent --add-service=cockpit # --zone=public
+sudo firewall-cmd --permanent --add-service=cockpit
 sudo systemctl reload firewalld
 
 # Installer CUPS
