80d728779186837103c36b9ed66d8ae19b97b3f7
at2f <91329492+at2f@users.noreply.github.com>
2022-08-23 17:12:03 +0200

    Create maintenance_manuelle_hebdomadaire.ps1

---
+++ e-formation/maintenance_manuelle_hebdomadaire.ps1
@@ -0,0 +1,6 @@
+winget upgrade --all
+
+# En mode administrateur
+mkdir 'C:\Program Files\Mozilla Firefox\distribution'
+# Télécharger paramètres entreprise Firefox
+wget --output 'C:\Program Files\Mozilla Firefox\distribution\policies.json' https...
