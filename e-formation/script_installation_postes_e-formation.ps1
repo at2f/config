531b1a90f23e93b65dae3c9fd950886b1de42740
at2f <91329492+at2f@users.noreply.github.com>
2022-08-23 15:39:12 +0200

    Create script_installation_postes_e-formation.ps1

---
+++ e-formation/script_installation_postes_e-formation.ps1
@@ -0,0 +1,41 @@
+# Mettre à jour les applications Microsoft Store (marche probablement pas vraiment)
+Get-CimInstance -Namespace "Root\cimv2\mdm\dmmap" -ClassName "MDM_EnterpriseModernAppManagement_AppManagement01" | Invoke-CimMethod -MethodName UpdateScanMethod
+
+# Installer les logiciels nécessaires
+winget install Microsoft.WindowsTerminal
+winget install Microsoft.PowerShell
+winget install Mozilla.Firefox
+winget install TheDocumentFoundation.LibreOffice
+winget install VideoLAN.VLC
+winget install 7zip.7zip
+winget install Adobe.Acrobat.Reader.64-bit
+
+# Désinstaller les logiciels non nécessaires
+winget uninstall Disney.37853FC22B2CE_6rarf9sa4v8jt
+winget uninstall Microsoft.BingNews_8wekyb3d8bbwe
+winget uninstall Microsoft.BingWeather_8wekyb3d8bbwe
+winget uninstall Microsoft.Edge
+winget uninstall Microsoft.549981C3F5F10_8wekyb3d8bbwe
+winget uninstall Microsoft.GamingApp_8wekyb3d8bbwe
+winget uninstall Microsoft.MicrosoftOfficeHub_8wekyb3d8bbwe
+winget uninstall Microsoft.MicrosoftSolitaireCollection_8wekyb3d8bbwe
+winget uninstall Microsoft.OneDriveSync_8wekyb3d8bbwe
+winget uninstall Microsoft.People_8wekyb3d8bbwe
+winget uninstall Microsoft.WindowsFeedbackHub_8wekyb3d8bbwe
+winget uninstall Microsoft.Xbox.TCUI_8wekyb3d8bbwe
+winget uninstall Microsoft.XboxGameOverlay_8wekyb3d8bbwe
+winget uninstall Microsoft.XboxGamingOverlay_8wekyb3d8bbwe
+winget uninstall Microsoft.XboxIdentityProvider_8wekyb3d8bbwe
+winget uninstall Microsoft.XboxSpeechToTextOverlay_8wekyb3d8bbwe
+winget uninstall Microsoft.ZuneMusic_8wekyb3d8bbwe
+winget uninstall Microsoft.ZuneVideo_8wekyb3d8bbwe
+winget uninstall Microsoft.OneDrive
+winget uninstall SpotifyAB.SpotifyMusic_zpdnekdrzrea0
+
+# Afficher les extensions de fichiers
+# Windows Terminal = Application par défaut
+
+# En mode administrateur
+mkdir 'C:\Program Files\Mozilla Firefox\distribution'
+# Télécharger paramètres entreprise Firefox
+wget --output 'C:\Program Files\Mozilla Firefox\distribution\policies.json' https...
