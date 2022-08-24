ea4eb06b44bab56fc3651389886e0ecd8369f776
at2f <91329492+at2f@users.noreply.github.com>
2022-08-24 14:37:32 +0200

    Update corrections.sh

--- cyber-base/Postes usagers/divers/corrections.sh
+++ cyber-base/Postes usagers/divers/corrections.sh
@@ -6,6 +6,12 @@
 echo 'Appliquation des corrections…'
 ### AJOUTER LES CORRECTIONS APRÈS CETTE LIGNE ###
 
+# Installer le support des systèmes de fichier iOS
+sudo apt install -y ifuse
+# Installer le support des images HEIF
+sudo apt install -y heif-gdk-pixbuf heif-thumbnailer
+# Définir la visionneuse d’images (Eye Of GNOME) comme logiciel par défaut pour ouvrir les images HEIF
+xdg-mime default org.gnome.eog.desktop image/heic
 
 ### AJOUTER LES CORRECTIONS AVANT CETTE LIGNE ###
 echo 'Corrections terminées.'
