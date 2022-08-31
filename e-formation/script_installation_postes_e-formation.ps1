f75f112847b52dd43817c849c627522f12840d43
at2f <91329492+at2f@users.noreply.github.com>
2022-08-31 15:19:05 +0200

    Update script_installation_postes_e-formation.ps1

--- e-formation/script_installation_postes_e-formation.ps1
+++ e-formation/script_installation_postes_e-formation.ps1
@@ -1,10 +1,7 @@
-# Mettre à jour les applications Microsoft Store (marche probablement pas vraiment)
-Get-CimInstance -Namespace "Root\cimv2\mdm\dmmap" -ClassName "MDM_EnterpriseModernAppManagement_AppManagement01" | Invoke-CimMethod -MethodName UpdateScanMethod
-
 # Installer les logiciels nécessaires
 winget install Microsoft.WindowsTerminal
 winget install Microsoft.PowerShell
-winget install Mozilla.Firefox
+winget install Mozilla.Firefox.ESR
 winget install TheDocumentFoundation.LibreOffice
 winget install VideoLAN.VLC
 winget install 7zip.7zip
@@ -33,7 +30,7 @@ winget uninstall Microsoft.OneDrive
 winget uninstall SpotifyAB.SpotifyMusic_zpdnekdrzrea0
 
 # Afficher les extensions de fichiers
-# Windows Terminal = Application par défaut
+reg add HKCU\Software\Microsoft\Windows\CurrentVersion\Explorer\Advanced /v HideFilesExt /t REG_DWORD /d 0 /f
 
-# En mode administrateur
+# Créer le dossier de configuration des paramètres d’entreprise de Firefox
 mkdir 'C:\Program Files\Mozilla Firefox\distribution'
