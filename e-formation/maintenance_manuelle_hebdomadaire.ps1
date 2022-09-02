51340ec05057672dc50428a960ab99748fe3fb05
at2f <91329492+at2f@users.noreply.github.com>
2022-09-02 17:32:05 +0200

    Update maintenance_manuelle_hebdomadaire.ps1

--- e-formation/maintenance_manuelle_hebdomadaire.ps1
+++ e-formation/maintenance_manuelle_hebdomadaire.ps1
@@ -1,4 +1,8 @@
 winget upgrade --all
 
-# Télécharger paramètres entreprise Firefox
+# Télécharger les paramètres d’entreprise de Mozilla Firefox
 wget --output 'C:\Program Files\Mozilla Firefox\distribution\policies.json' https...
+
+# Télécharger les paramètres d’entreprise de Google Chrome
+
+# Télécharger les paramètres d’entreprise de Microsoft Edge
