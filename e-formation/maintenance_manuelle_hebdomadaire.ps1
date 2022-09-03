62c3b1e2f69812189dfcc3e3190c1e043e2f4d5d
at2f <91329492+at2f@users.noreply.github.com>
2022-09-03 12:28:16 +0200

    Update maintenance_manuelle_hebdomadaire.ps1

--- e-formation/maintenance_manuelle_hebdomadaire.ps1
+++ e-formation/maintenance_manuelle_hebdomadaire.ps1
@@ -1,10 +1,12 @@
 winget upgrade --all
 
 # Télécharger les paramètres d’entreprise de Mozilla Firefox
-wget --output 'C:\Program Files\Mozilla Firefox\distribution\policies.json' https...
+Invoke-WebRequest -Uri https://raw.githubusercontent.com/at2f/config/main/e-formation/firefox/policies.json -OutFile 'C:\Program Files\Mozilla Firefox\distribution\policies.json'
 
-# Télécharger les paramètres d’entreprise de Google Chrome
-reg import
+# Télécharger les paramètres d’entreprise de Google Chrome & appliquer les changements au registre
+Invoke-WebRequest -Uri https://raw.githubusercontent.com/at2f/config/main/e-formation/chrome/policies_chrome.reg -OutFile "$env:TEMP\policies_chrome.reg"
+reg import "$env:TEMP\policies_chrome.reg"
 
-# Télécharger les paramètres d’entreprise de Microsoft Edge
-reg import
+# Télécharger les paramètres d’entreprise de Microsoft Edge & appliquer les changements au registre
+Invoke-WebRequest -Uri https://raw.githubusercontent.com/at2f/config/main/e-formation/edge/policies_edge.reg -OutFile "$env:TEMP\policies_edge.reg"
+reg import "$env:TEMP\policies_edge.reg"
