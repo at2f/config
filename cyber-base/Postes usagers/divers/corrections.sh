7a5301e05410f55ddac68abb73abeb489c51d4f4
at2f <91329492+at2f@users.noreply.github.com>
2022-09-03 21:23:23 +0200

    Update corrections.sh

--- cyber-base/Postes usagers/divers/corrections.sh
+++ cyber-base/Postes usagers/divers/corrections.sh
@@ -6,7 +6,17 @@
 echo 'Appliquation des corrections…'
 ### AJOUTER LES CORRECTIONS APRÈS CETTE LIGNE ###
 
+#!/usr/bin/bash
 
+# Supprimer l’installation actuelle de Firefox
+sudo rm -rf /usr/local/bin/Firefox/*
+sudo rm -rf ~/.mozilla
+
+(
+cd $(mktemp -d)
+wget https://raw.githubusercontent.com/at2f/config/main/cyber-base/Postes%20usagers/installation/modules/configurer_firefox.sh
+bash ./configurer_firefox.sh
+)
 
 ### AJOUTER LES CORRECTIONS AVANT CETTE LIGNE ###
 echo 'Corrections terminées.'
