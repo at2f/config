4955e6757970a5e31a20e07513a3fb44858591f6
at2f <91329492+at2f@users.noreply.github.com>
2022-09-08 17:56:14 +0200

    Update maintenance_manuelle_hebdomadaire.ps1

--- e-formation/divers/maintenance_manuelle_hebdomadaire.ps1
+++ e-formation/divers/maintenance_manuelle_hebdomadaire.ps1
@@ -1,1 +1,1 @@
-Start-Process powershell -verb runas -ArgumentList "-file C:\Users\Admin\script_maintenance-a-elever.ps1"
+Start-Process powershell -verb runas -ArgumentList "-file $env:USERPROFILE\script_maintenance-a-elever.ps1"
